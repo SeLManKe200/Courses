8465e2fa1feeddd4310b8d307a97ff098443e2a6
ahmet <ahmetselmanakarisan@gmail.com>
2022-05-17 22:38:24 +0300

    a

--- Label/test.java
+++ Label/test.java
@@ -1,6 +1,7 @@
 package Label;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class test {
 
@@ -9,7 +10,8 @@ public class test {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
         frame.setVisible(true);
-
+        JLabel label = new JLabel("Bro , do you even code?");
+        frame.add(label);
 
 
     }
